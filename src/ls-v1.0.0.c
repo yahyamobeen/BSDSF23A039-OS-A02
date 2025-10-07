68772a7cd8fdc43bdef42e0048f7579cf62136a2
Yahyamobeen <yahyamobeen6@gmail.com>
2025-10-06 23:03:37 -0400

    feat(v1.1.0): Add required headers for long listing format
    - Added sys/stat.h for file metadata
    - Added pwd.h and grp.h for user/group resolution
    - Added time.h for timestamp formatting
    - Added getopt.h for command-line parsing

--- src/ls-v1.0.0.c
+++ src/ls-v1.0.0.c
@@ -14,7 +14,17 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/stat.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <getopt.h>
 extern int errno;
 
 void do_ls(const char *dir);
